218fea598157a309a7080c6807b5fe157ce721cf
Lucas Santana <ln.santana08@gmail.com>
2023-07-22 02:53:13 -0300

    feat: add cors origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: 'https://perciclando.vercel.app',
     methods: '*',
     allowedHeaders: '*',
   });
